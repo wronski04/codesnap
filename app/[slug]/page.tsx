80c92d195a34e312967756bea0d485ebe48c89b7
wronski04 <150080720+wronski04@users.noreply.github.com>
2026-03-20 16:06:20 +0100

    feat: supabase integration - fetch and create pads

--- app/[slug]/page.tsx
+++ app/[slug]/page.tsx
@@ -1,10 +1,30 @@
+import { supabase } from "@/lib/supabase"
+
 export default async function PadPage({ params }: { params: Promise<{ slug: string }> }) {
   const { slug } = await params
+  const { data } = await supabase
+    .from('pads')
+    .select('*')
+    .eq('slug', slug)
+    .single() 
+
+    let pad = data
+
+    if (!data) {
+      const { data: newPad } = await supabase
+      .from('pads')
+      .insert({ slug })
+      .select('*')
+      .single()
+      pad = newPad
+    }
+    
   
   return (
     <div style={{ padding: "2rem" }}>
       <h1>Pad: {slug}</h1>
       <p>To będzie collaborative editor</p>
+      <p>{pad?.content}</p>
     </div>
   )
 }
